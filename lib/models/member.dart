a1f3c2241f3d6328d7f8dd41e96ac090467fefa7
Mobin <mobin.aranii@gmail.com>
2023-08-14 08:29:14 +0330

    member class edited

--- lib/models/member.dart
+++ lib/models/member.dart
@@ -1,13 +1,25 @@
+import 'dart:core';
+
 class Member {
   Member(
-      {required this.id ,required this.name,
-      required this.family,
-      required this.userName,
-      required this.isAdmin});
+      {required this.id,
+        required this.name,
+        required this.fatherName,
+      required this.meliNumber,
+      required this.shenasnameNumber,
+      required this.address,
+      required this.phone,
+      required this.moblie,
+
+      });
 
   final String id;
   final String name;
-  final String family;
-  final String userName;
-  final bool isAdmin;
+  final String fatherName;
+  final int meliNumber;
+  final int shenasnameNumber;
+  final String address;
+  final int phone;
+  final int moblie;
+
 }
